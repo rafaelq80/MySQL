12e6dffccd4d0041d54079ca36f99f631bcb8284
Rafael Queiroz <rafaelproinfo@gmail.com>
2021-05-20 20:07:55 -0300

    Commit inicial

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # MySQL
 Conteúdos das aulas de MySQL - Generation Brazil
+
+## exemplo_aula_02
+Exemplo apresentado na live code (db_quitanda) com algumas consultas adicionais (Bônus)
+
+## exemplo_aula_03
+Exemplo + DER para a live code da aula 03
---
+++ exemplo_aula_02/quitanda.sql
@@ -0,0 +1,70 @@
+-- Criar Banco de Dados
+create database db_quitanda;
+
+-- Acessar o Banco de Dados
+use db_quitanda;
+
+-- Criar Tabela
+create table tb_produtos(
+id bigint auto_increment,
+nome varchar(255) not null,
+preco decimal not null,
+primary key (id)
+);
+
+-- Insere dados na tabela
+insert into tb_produtos(nome, preco)  values ("tomate", 8.00);
+insert into tb_produtos(nome, preco)  values ("maçã", 5.00);
+insert into tb_produtos(nome, preco)  values ("laranja", 10.00);
+insert into tb_produtos(nome, preco)  values ("banana", 12.00);
+insert into tb_produtos(nome, preco)  values ("uva", 30.00);
+insert into tb_produtos(nome, preco)  values ("pêra", 3.00);
+
+/* Visualizar os dados*/
+
+-- Visualiza todos os dados
+select * from tb_produtos;
+
+-- Visualiza somente o nome e o preço
+select nome, preco from tb_produtos;
+
+-- Visualiza o produto cujo id é igual a 1
+select * from tb_produtos where id = 1;
+
+-- Visualiza todos os produtos cujo preço seja maior do que 5
+select * from tb_produtos where preco > 5.00;
+
+-- Visualiza todos os produtos cujo nome seja maçã
+select * from tb_produtos where nome = "maçã";
+
+/* Alterações na Estrutura da Tabela*/
+
+-- Adiciona um novo Atributo na Tabela
+alter table tb_produtos add descricao varchar(255); 
+
+-- Atualiza o tipo de um Atributo da Tabela
+alter table tb_produtos modify preco decimal(5,2);
+
+-- Remove um atributo da Tabela
+alter table tb_produtos drop descricao; 
+
+
+/* Alterações nos Dados da Tabela*/
+
+-- Atualiza o atributo preço na tabela, cujo id seja igual a 1
+update tb_produtos set preco = 10.00 where id = 1;
+
+-- Deleta o registro da tabela, cujo id seja igual a 2
+delete from tb_produtos where id = 2;
+
+
+/* Bônus */
+
+-- Esqueceu de definir a chave primária da tabela
+ALTER TABLE tb_produtos ADD PRIMARY KEY (id);
+
+-- Modifica o nome de um Atributo da Tabela
+alter table tb_produtos change nome nomeProduto integer;
+
+-- Apaga todos os dados da tabela (Não é possível desfazer)
+delete from tb_produtos;
